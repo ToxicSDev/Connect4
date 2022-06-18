60708b3ab6c17d94289278ec81b114fb79c09332
ToxicSDev <72464460+ToxicSDev@users.noreply.github.com>
2022-06-18 10:35:07 +0300

    Basic connect4 methods

--- main.cpp
+++ main.cpp
@@ -1,4 +1,42 @@
-//
-// Created by kaloy on 6/17/2022.
-//
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+class Connect4{
+protected:
+    int height;
+    int width;
+    char** board;
+
+public:
+    Connect4(int h, int w){
+        height = h;
+        width = w;
+        board = new char*[height];
+        for(int i = 0; i < height; i++){
+            board[i] = new char[width];
+        }
+        for(int i = 0; i < height; i++){
+            for(int j = 0; j < width; j++){
+                board[i][j] = ' ';
+            }
+        }
+    }
+    ~Connect4(){
+        for(int i = 0; i < height; i++){
+            delete[] board[i];
+        }
+        delete[] board;
+    }
+
+    void printBoard() {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                cout << board[i][j];
+            }
+            cout << endl;
+        }
+    }
+
+};
