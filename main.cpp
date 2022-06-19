25cc7243a4cb0abf80ebc0e47bb4ee99b35a7f6f
ToxicSDev <72464460+ToxicSDev@users.noreply.github.com>
2022-06-19 13:28:05 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -30,11 +30,28 @@ public:
         }
     }
 
+    void resetBoard() {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                board[i][j] = ' ';
+            }
+        }
+    }
+
+    bool isValid(int col){
+        if(col < 0 || col >= width || heights[col] == height){
+            return false;
+        }
+
+        return true;
 };
 
 class Connect4 : public Board{
+private:
+    char player;
+    char computer;
 public:
-    Connect4(int height, int width) : Board(height, width) {
+    Connect4(int height, int width, char player, char computer) : Board(height, width) {
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
                 this->board[i][j] = ' ';
@@ -43,12 +60,14 @@ public:
         for (int i = 0; i < width; i++) {
             this->heights[i] = 0;
         }
+        this->player = player;
+        this->computer = computer;
     }
 
-    bool checkWin(char player) {
+    bool checkWin(char inputChar) {
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                if (board[i][j] == player) {
+                if (board[i][j] == inputChar) {
                     // horizontal check
                     if (j + 3 < width) {
                         if (board[i][j] == board[i][j + 1] && board[i][j] == board[i][j + 2] &&
@@ -84,47 +103,24 @@ public:
         return false;
     }
 
-    bool checkDraw() {
-        for (int i = 0; i < width; i++) {
-            if (heights[i] < height) {
-                return false;
-            }
-        }
-        return true;
+    int calculateMaxRounds() {
+        return (height * width);
     }
 
-    void makeMove(int column, char player) {
+    void makeMove(int column, char inputChar) {
         if (heights[column] < height) {
-            board[heights[column]][column] = player;
+            board[heights[column]][column] = inputChar;
             heights[column]++;
         }
     }
 
+    void resetGame() {
+        this->resetBoard();
+        delete [] heights;
+        heights = new int[width];
+    }
 };
 
 int main() {
-    Connect4 game(6, 7);
-    game.printBoard();
-
-    game.makeMove(0, 'X');
-    game.printBoard();
-
-    game.makeMove(1, 'O');
-    game.printBoard();
-
-    game.makeMove(2, 'X');
-    game.printBoard();
-
-    game.makeMove(2, 'X');
-    game.printBoard();
-
-    game.makeMove(2, 'X');
-    game.printBoard();
-
-    cout << game.checkWin('X') << endl;
-
-    game.makeMove(2, 'X');
-    game.printBoard();
-
-    cout << game.checkWin('X') << endl;
+    Connect4 game(6, 7, 'X', 'O');
 }
