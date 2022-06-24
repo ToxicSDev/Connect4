a3d2f7a26fa07812d6a613d666142f3349ff8daf
ToxicSDev <72464460+ToxicSDev@users.noreply.github.com>
2022-06-24 15:06:48 +0300

    Picking best move horizontally

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 #include <string>
+#include <limits>
+#include <vector>
+
+int MAX_INT = std::numeric_limits<int>::max();
+int MIN_INT = std::numeric_limits<int>::min();
 
 using namespace std;
 
@@ -22,9 +27,11 @@ protected:
 
 public:
     void printBoard() {
+        cout << "Board:" << endl;
         for (int i = height - 1; i >= 0; i--) {
+            cout << "|";
             for (int j = 0; j < width; j++) {
-                cout << board[i][j];
+                cout << board[i][j] << "|";
             }
             cout << endl;
         }
@@ -102,6 +109,14 @@ public:
         return *this;
     }
 
+    int getHeight(){
+        return height;
+    }
+
+    int getWidth(){
+        return width;
+    }
+
     bool checkWin(char inputChar) {
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
@@ -171,21 +186,134 @@ public:
         return -1;
     }
 
-    void calculateScore(char inputChar){
-        // todo: calculate score for minmax algorithm
+    char getOpponent(char inputChar){
+        if(inputChar == player){
+            return computer;
+        }
 
+        return player;
+    }
+
+    char* getRow(int pos) {
+        char* row = new char[width];
+        for (int i = 0; i < width; i++) {
+            row[i] = board[pos][i];
+        }
+
+        return row;
+    }
+
+    char* getWindow(const char* row, int offset){
+        char* window = new char[4];
+        for (int i = 0; i < 4; i++) {
+            window[i] = row[(i + offset)];
+        }
+
+        return window;
+    }
+
+    int countInWindow(const char* window, char inputChar){
+        int count = 0;
+        for (int i = 0; i < 4; i++) {
+            if(window[i] == inputChar){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    int evaluateWindow(const char* window, char inputChar) {
+        int score = 0;
+        char opponent = getOpponent(inputChar);
+
+        if(countInWindow(window, inputChar) == 4){
+            score += 100;
+        }else if(countInWindow(window, inputChar) == 3 && countInWindow(window, ' ') == 1) {
+            score += 5;
+        }else if(countInWindow(window, inputChar) == 2 && countInWindow(window, ' ') == 2) {
+            score += 2;
+        }
+
+        if(countInWindow(window, opponent) == 4) {
+            score -= 4;
+        }
+
+        return score;
+    }
+
+    int getScore(char inputChar){
+        int score = 0;
+        for(int i = 0; i < height; i++){
+            char* row = getRow(i);
+            for(int j = 0; j < width - 3; j++){
+                char* window = getWindow(row, j);
+                score += evaluateWindow(window, inputChar);
+            }
+        }
+
+        return score;
+    }
+
+
+    vector<int> getValidMoves(){
+        vector<int> validMoves;
+        for(int i = 0; i < width; i++){
+            if(isValid(i)){
+                validMoves.push_back(i);
+            }
+        }
+        return validMoves;
+    }
+
+    int getNextOpenRow(int col){
+        for(int i = heights[col]; i < height; i++){
+            if(board[i][col] == ' '){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    int bestMove(char inputChar){
+        vector<int> validLocations = getValidMoves();
+
+        int bestScore = MIN_INT;
+        unsigned int bestCol;
+
+        for(unsigned int i = 0; i < validLocations.size(); i++){
+            int row = getNextOpenRow(validLocations[i]);
+            if(row != -1){
+                Connect4 temp(*this);
+                temp.makeMove(validLocations[i], inputChar);
+                int score = temp.getScore(inputChar);
+                if(score > bestScore){
+                    bestScore = score;
+                    bestCol = i;
+                }
+            }
+        }
+
+        return bestCol;
     }
 };
 
 int main() {
     Connect4 game(6, 7, 'X', 'O');
 
-    game.makeMove(0, 'X');
-    game.makeMove(0, 'X');
+    game.makeMove(0, 'O');
+    game.makeMove(1, 'O');
 
-    cout << game.winningMove('X') << endl;
+    cout << game.winningMove('O') << endl;
 
-    game.makeMove(0, 'X');
+    game.makeMove(3, 'O');
+    game.makeMove(3, 'X');
 
-    cout << game.winningMove('X') << endl;
+    cout << game.winningMove('O') << endl;
+    game.printBoard();
+
+    int col = game.bestMove('O');
+
+    cout << "Best move: " << col << endl;
+
+
 }
